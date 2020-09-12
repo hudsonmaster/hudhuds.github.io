9ed6e9e73a5396842381b1580d63c3add1a40c79
hudsonmaster <71190179+hudsonmaster@users.noreply.github.com>
2020-09-12 17:45:30 -0300

    Index.html

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>PÁGINA </title>
+	<link rel="stylesheet" href="css/style.css" media="screen"/>
+	<script src="js/jquery-2.1.3.js"></script>
+    
+
+</head>
+<body>
+	<div id="container">
+		<div id="loader"><img src="http://200.229.103.50:8080/830.gif"></div>
+		<div id="content">
+			<style type="text/css">
+			
+			</style>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+			<script>
+			$(document).ready (function (){
+			$('#menubt').click (function (){
+			$('.li').slideToggle (1000);
+			});
+			
+			});
+			var oi=prompt ('seu nome: ','')
+			</script>
+			</head>
+			<body style="background:;">
+			<div id="cab"style="position:fixed!important;">
+			<div id="menubt"style="background:;">
+			<!-- menu -->
+			<svg  id="oi"  aria-hidden="true" data-prefix="fas" data-icon="address-card" class="svg-inline--fa fa-address-card fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+			<path id="fil" fill="currentColor" d="M528 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-352 96c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H86.4C74 384 64 375.4 64 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2zM512 312c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16zm0-64c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16zm0-64c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16z">
+			</path>
+			</svg>
+
+			</div>
+			</div>
+			<ul class="li"style="height:1000px;"></br>
+			<img src="#" id="img"/>
+			<span id="sp" style="color:blue;font-size:30px;" ><script >document.write (oi);</script></span>
+			</center>
+			<br></br>
+			<hr>
+			<span>Casa</span>
+			<hr>
+			<span>Fotos</span>
+			<hr>
+			<span>Videos</span>
+			<hr>
+			<span>Mel</span>
+			<hr>
+			
+			<span><a href="News.php"> Hudson</a></span>
+			<hr>
+			</ul>
+			</div>
+			<div id="con">
+			<h1>top</h1>bn
+			
+			gjvnvhng
+			no dia 06/06/20<br>
+			Estava em meu serviço trabalhando até que chega amim um elemento
+			que atende pelo nome (anónimo)
+			este mesmo homem que entrou me ameaçando dizendo farei você pasar vergonha 
+			
+			<p><br>
+			</div>
+			<div id="rp">
+			
+			<a href="#top" class="glyphicon glyphicon-chevron-up"></a>
+			<style>
+			
+			a[href="#top"]{
+			padding:10px;
+			position:fixed;
+			top: 90%;
+			right:40px;
+			display:none;
+			font-size: 30px;
+			}
+			a[href="#top"]:hover{
+			text-decoration:none;
+			}
+			</style>
+			
+			<script type="text/javascript">
+			// Este evendo é acionado após o carregamento da página
+			jQuery(window).load(function() {
+			//Após a leitura da pagina o evento fadeOut do loader é acionado, esta com delay para ser perceptivo em ambiente fora do servidor.
+			jQuery("#loader").delay(1000).fadeOut("slow");
+			});
+			
+			$(document).ready(function(){
+			$(window).scroll(function(){
+			if ($(this).scrollTop() > 100) {
+			$('a[href="#top"]').fadeIn();
+			} else {
+			$('a[href="#top"]').fadeOut();
+			}
+			});
+			
+			$('a[href="#top"]').click(function(){
+			$('html, body').animate({scrollTop : 0},800);
+			return false;
+			});
+			});
+			Window.requestAnimate.Frames({});
+			ab=pop (123);
+			for(ab <== 0;ab ++);
+			document.write (bubles- fill_pop {
+				while (ab < 100;
+			}
+			</script>
+			<img src="http://200.229.103.50:8080/facebook.png"style="width:100px;">
+			 <a href="http://192.168.1.64:8080/New_Ajax_Image_Upload/New_Ajax_Image_Upload/">hh</a>
+			</div>
+			<p><br></p></br><div>
+			<a href="http://localhost:8080/list.py">linckk</a>
+			</div>
+			<div>
+			<a href="http://localhost:8080/list.py">linckk</a>
+			</div><div>
+			<a href="http://localhost:8080/list.py">linckk</a>
+			</div><div>
+			<a href="http://localhost:8080/list.py">linckk</a>
+			</div><div>
+			<a href="http://localhost:8080/list.py">linckk</a>
+			</div><div>
+			<a href="http://localhost:8080/list.py">linckk</a>
+			</div><div><p>
+			<a href="http://localhost:8080/list.py">linckk</a>
+			</div><div><p>
+			<a href="http://localhost:8080/list.py">linckk</a>
+			</div>
+			</body>
+			</html>
+			
+			</div>
+	</div>
+</body>
+</html>
